64288b3477dc4848425d544886fb54f30b2cad22
Marzeu <marzahell@hotmail.com>
2022-11-30 21:03:08 -0300

    metodo subtração

--- Program.cs
+++ Program.cs
@@ -23,6 +23,23 @@ namespace Calculator
             Console.WriteLine("");
 
             Console.WriteLine($"O resultado da soma é {v1 + v2}");
+            Console.ReadKey();
+        }
+
+        static void Subtracao(){
+
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.WriteLine($"O resultado da soma é {v1 - v2}");
+            Console.ReadKey();
         }
     }
 }
